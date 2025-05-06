052cc86df8a8b118a5b56c70da1d51bd0ecd4223
WongJF05 <141015746+WongJF05@users.noreply.github.com>
2025-05-07 01:47:14 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,21 +1,12 @@
-<?PHP 
+<?php
+$host = 'database-1.cdmd8ockimnp.us-east-1.rds.amazonaws.com';
+$dbname = 'ecommerce';
+$user = 'admin';
+$password = 'your_rds_password';
 
+$condb = new mysqli($host, $user, $password, $dbname);
 
-
-$nama_host="localhost";
-
-$user_sql="root";
-
-$pass_sql="";
-
-$nama_db="e-commerce";
-
-
-$condb=new mysqli($nama_host,$user_sql,$pass_sql,$nama_db);
-
-
-if ($condb->connect_error) 
-{
-    die("Access Denied" . $condb->connect_error);
-} 
+if ($condb->connect_error) {
+    die("Connection failed: " . $condb->connect_error);
+}
 ?>
